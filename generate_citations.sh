5ba3b0819b4249543f21ce04e5a484db02b6b958
EC2 Default User <ec2-user@ip-172-31-15-153.us-west-2.compute.internal>
2023-02-17 02:49:05 +0000

    Update run script

---
+++ generate_citations.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+current_date=$(date +"%Y%m%d")
+
+echo "Starting Citation Generation - $current_date"
+echo "    Log file: /home/ec2-user/SPARC-Citations/logs/run_$current_date.log"
+
+python3 /home/ec2-user/SPARC-Citations/data_citations.py >> /home/ec2-user/SPARC-Citations/logs/run_$current_date.log 2>&1
+
+python3 /home/ec2-user/SPARC-Citations/output_citations.py >> /home/ec2-user/SPARC-Citations/logs/run_$current_date.log 2>&1
+
+/home/ec2-user/SPARC-Citations/pandoc-3.1/bin/pandoc --pdf-engine wkhtmltopdf /home/ec2-user/SPARC-Citations/citation_markdown.md -o /home/ec2-user/SPARC-Citations/citation_markdown.pdf 
